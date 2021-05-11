13542abcca5d7782e574cd0d6bb27a862205b343
Arsenii Dragunkin <arsenii11@MacBook-Pro-Arsenii.local>
2021-05-11 19:06:05 +0300

    Bug fix and white background

---
+++ app/src/main/java/com/example/finances/diagram_colors.java
@@ -0,0 +1,26 @@
+package com.example.finances;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.finances.MainActivity;
+import com.example.finances.R;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.LineScatterCandleRadarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+public class diagram_colors extends AppCompatActivity {
+
+
+public void setDiagram_colors() {
+
+
+}
+
+    public static int[] Diagram_colors = { R.color.q,R.color.w,R.color.r,R.color.t,R.color.y,R.color.u,R.color.i,R.color.o};
+
+
+}
