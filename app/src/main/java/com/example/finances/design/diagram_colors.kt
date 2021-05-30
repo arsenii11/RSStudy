b9e64a21946c176650e5a7d26a84c749e49778b6
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2021-05-31 00:16:04 +0300

    "библиотека для выпадающего списка 1"

--- app/src/main/java/com/example/finances/design/diagram_colors.kt
+++ app/src/main/java/com/example/finances/design/diagram_colors.kt
@@ -1,26 +1,22 @@
-package com.example.finances.design;
+package com.example.finances.design
 
-import android.app.AppComponentFactory;
-import android.os.Bundle;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity
+import com.example.finances.R
 
-import androidx.appcompat.app.AppCompatActivity;
+class diagram_colors : AppCompatActivity() {
+    fun setDiagram_colors() {}
 
-import com.example.finances.MainActivity;
-import com.example.finances.R;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.data.LineScatterCandleRadarDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
-public class diagram_colors extends AppCompatActivity {
-
-
-public void setDiagram_colors() {
-
-
-}
-
-    public static int[] Diagram_colors = { R.color.q,R.color.w,R.color.r,R.color.t,R.color.y,R.color.u,R.color.i,R.color.o};
-
-
+    companion object {
+        @JvmField
+        var Diagram_colors = intArrayOf(
+            R.color.q,
+            R.color.w,
+            R.color.r,
+            R.color.t,
+            R.color.y,
+            R.color.u,
+            R.color.i,
+            R.color.o
+        )
+    }
 }
