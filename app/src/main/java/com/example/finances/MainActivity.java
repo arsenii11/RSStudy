c759b2995c5f1e08b7ad951a5616a9b2a2bac71a
Matvey Barbashov <matvieib@bk.ru>
2021-07-13 22:32:18 +0300

    Fix

--- app/src/main/java/com/example/finances/MainActivity.java
+++ app/src/main/java/com/example/finances/MainActivity.java
@@ -40,7 +40,6 @@ import static com.example.finances.ui.Statistics.StatisticsFragment.APP_PREFEREN
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
-
     private static final int MY_PERMISSIONS_REQUEST_READ_MEDIA = 1;
     public static boolean ALLOW_ADD_TO_CALENDAR = false;
 
@@ -51,21 +50,14 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_calendar, R.id.navigation_account)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
+
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-
-
-
 
         //Обновляем состояние будильника
         myAlarm();
@@ -73,16 +65,12 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         //изображение на toolbar
         setToolbarImage();
 
-
         ImageButton settings = findViewById(R.id.settings_bt);
-        settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
-                CustomIntent.customType(MainActivity.this,"fadein-to-fadeout");
-                finish();
-            }
+        settings.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+            CustomIntent.customType(MainActivity.this, "fadein-to-fadeout");
+            finish();
         });
 
         //регистрируем обработчик настроек
@@ -91,22 +79,22 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         prefs.registerOnSharedPreferenceChangeListener(this);
 
         //Проверка разрешений
-        if(checkSelfPermission(Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) requestPermission();
-        if(checkSelfPermission(Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) requestPermission();
-        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) requestPermission();
-        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) requestPermission();
+        if (checkSelfPermission(Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED)
+            requestPermission();
+        if (checkSelfPermission(Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED)
+            requestPermission();
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            requestPermission();
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            requestPermission();
 
         ImageButton toolbar_button = toolbar.findViewById(R.id.toolbar_image_button);
-        toolbar_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent AccOunt = new Intent(MainActivity.this, Account.class);
-                startActivity(AccOunt);
-            }
+        toolbar_button.setOnClickListener(v -> {
+            Intent Account = new Intent(MainActivity.this, Account.class);
+            startActivity(Account);
         });
 
     }
-
 
 
     @RequiresApi(api = Build.VERSION_CODES.P)
@@ -127,52 +115,34 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         setToolbarImage();
 
-       //Проверяем настройки
+        //Проверяем настройки
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        Boolean flagTheme = prefs.getBoolean("aa",false);
+        Boolean flagTheme = prefs.getBoolean("aa", false);
         String theme = prefs.getString("ThemeList", "");
-        if(flagTheme) {
+        if (flagTheme) {
             if (theme.equals("Bright")) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
             if (theme.equals("Dark")) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
             }
-        }
-        else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        } else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
     }
 
-        @Override
-    protected void onDestroy(){
+    @Override
+    protected void onDestroy() {
         super.onDestroy();
     }
-
-    private void readDataExternal() {
-        //Можно потом подумать, но у меня все работает и без него
-    }
-
-
-    @Override public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_MEDIA:
-            if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                readDataExternal(); }
-            break;
-            default:
-                break; } }
-
-
 
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
     }
-
 
 
     //блок чтения настроек
@@ -183,11 +153,11 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         String nickname = prefs.getString("Nickname", "");
         TextView Nickname = findViewById(R.id.name);
         try {
-        Nickname.setText(nickname);
-        if (nickname.isEmpty()) {
-            Nickname.setText("Nickname");
-        }}
-        catch (NullPointerException e){
+            Nickname.setText(nickname);
+            if (nickname.isEmpty()) {
+                Nickname.setText("Nickname");
+            }
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
         //устанавливаем email
@@ -199,36 +169,33 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
             if (email.isEmpty()) {
                 Email.setText("Not indicated");
             }
-        }  catch (NullPointerException e){
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
 
         //устанавливаем место учебы
         String EduPLace = prefs.getString("Educational institution", "");
-        TextView eduplace = findViewById(R.id.nickname);
+        TextView eduPlace = findViewById(R.id.nickname);
         try {
-            eduplace.setText(EduPLace);
-            if (EduPLace.isEmpty()) {
-                eduplace.setText("Not indicated");
-            }
-        } catch (NullPointerException e){
+            eduPlace.setText(EduPLace);
+            if (EduPLace.isEmpty())
+                eduPlace.setText("Not indicated");
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
 
 
         //Проверяем настройки
 
-        Boolean flagTheme = prefs.getBoolean("aa",false);
+        Boolean flagTheme = prefs.getBoolean("aa", false);
         String theme = prefs.getString("ThemeList", "");
-        if(flagTheme) {
-            if (theme.equals("Bright")) {
+        if (flagTheme) {
+            if (theme.equals("Bright"))
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-            }
-            if (theme.equals("Dark")) {
+            if (theme.equals("Dark"))
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            }
-        }
-        else {AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);}
+        } else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
 
 
         //активируем добавление событий в календарь
@@ -236,16 +203,16 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     //Запрашиваем разрешения
-    private void requestPermission(){
+    private void requestPermission() {
         String[] permissions = new String[]{Manifest.permission.READ_CALENDAR,
                 Manifest.permission.WRITE_CALENDAR,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
-        ActivityCompat.requestPermissions(this,permissions,1);
+        ActivityCompat.requestPermissions(this, permissions, 1);
     }
 
-    public void setToolbarImage(){
+    public void setToolbarImage() {
         SharedPreferences accountPhoto = getSharedPreferences(APP_PREFERENCES_Path, Context.MODE_PRIVATE);
         String FilePath = accountPhoto.getString("key1", "");
         File Photo = new File(FilePath);
