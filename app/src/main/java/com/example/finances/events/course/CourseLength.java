8d58c310fe060bed18d39fcf2895266009a26872
Matvey Barbashov <matvieib@bk.ru>
2021-07-15 12:04:40 +0300

    Починено добавление уроков при создании курса

--- app/src/main/java/com/example/finances/events/course/CourseLength.java
+++ app/src/main/java/com/example/finances/events/course/CourseLength.java
@@ -2,7 +2,6 @@ package com.example.finances.events.course;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,93 +16,79 @@ import com.example.finances.R;
 import com.example.finances.database.Course;
 import com.example.finances.database.DBHelper;
 import com.example.finances.events.lesson.NewLessonActivity;
-import com.google.android.material.snackbar.Snackbar;
 
 import maes.tech.intentanim.CustomIntent;
 
 
 public class CourseLength extends AppCompatActivity {
-    ImageButton close;
-    Button next;
-    Spinner spinner;
-    String cn;
+    ImageButton close; //Кнопка закрытия
+    Button next; //кнопка дальше
+    Spinner spinner; //меню выбора количества уроков в неделю
+    int lessons; //Выбранное количество уроков в неделю
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_length);
 
-        //final EditText contentEditText = findViewById(R.id.editTextCourseLength);
+        spinner = findViewById(R.id.spinner); //получаем из View меню для выбора количества уроков в неделю
 
-        spinner = findViewById(R.id.spinner);
+        //Создаем новый адаптер для меню выбора количества уроков в неделю с предопределенными значениями
         String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, numbers);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+
+        spinner.setAdapter(adapter); //Устанавливаем адптер в меню
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                cn = String.valueOf(position);
-
+                lessons = position;
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                cn = "0";
+                lessons = 0;
             }
         });
 
         next = findViewById(R.id.buttonNext2);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                //cn = contentEditText.getText().toString();
-                int content = Integer.parseInt(cn);
-                Log.e("LESSONS", cn);
-                Log.e("LESSONS", String.valueOf(content));
-                Intent i = getIntent();
-                Course course = new Course();
-                course.setName(i.getStringExtra("COURSE_NAME"));
+        next.setOnClickListener(v -> {
+            Intent i = getIntent();
+            Course course = new Course();
+            course.setName(i.getStringExtra("COURSE_NAME"));
 
-                DBHelper dbh = new DBHelper(getApplicationContext());
-                long courseId = dbh.insertCourse(course);
-                if( courseId != -1 && !cn.equals("0")) {
-                    course = dbh.getCourse(courseId);
-                    Intent intent = new Intent(CourseLength.this, NewLessonActivity.class);
-                    intent.putExtra("COURSE_ID", course.getId());
-                    intent.putExtra("LESSONS", course.getLessons());
-                    intent.putExtra("CURRENT_LESSON", 0);
-                    startActivity(intent);
-                }
-                else {
-
-
-                    Intent intent = new Intent(CourseLength.this, MainActivity.class);
-                    startActivity(intent);
-                }
-                CustomIntent.customType(CourseLength.this,"left-to-right");
-                finish();
+            DBHelper dbh = new DBHelper(getApplicationContext());
+            long courseId = dbh.insertCourse(course);
+            if( courseId != -1 && lessons != 0) {
+                course = dbh.getCourse(courseId);
+                Intent intent = new Intent(CourseLength.this, NewLessonActivity.class);
+                intent.putExtra("COURSE_ID", course.getId());
+                intent.putExtra("LESSONS", lessons);
+                intent.putExtra("CURRENT_LESSON", 0);
+                startActivity(intent);
             }
+            else {
+                Intent intent = new Intent(CourseLength.this, MainActivity.class);
+                startActivity(intent);
+            }
+            CustomIntent.customType(CourseLength.this,"left-to-right");
+            finish();
         });
 
 
         close = findViewById(R.id.closeButton2);
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        close.setOnClickListener(v -> {
+            Intent intent = new Intent(CourseLength.this, MainActivity.class);
+            startActivity(intent);
+            CustomIntent.customType(CourseLength.this,"fadein-to-fadeout");
+            finish();
 
-                Intent intent = new Intent(CourseLength.this, MainActivity.class);
-                startActivity(intent);
-                CustomIntent.customType(CourseLength.this,"fadein-to-fadeout");
-                finish();
-
-                if(CourseName.instance != null) {
-                    try {
-                        CourseName.instance.finish();
-                    } catch (Exception e) {}
-                }
-
+            if(CourseName.instance != null) {
+                try {
+                    CourseName.instance.finish();
+                } catch (Exception e) {}
             }
+
         });
     }
 }
