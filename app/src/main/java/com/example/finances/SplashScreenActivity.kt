3b6416e664046c324d58a467d01a9921164a56f6
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2021-07-20 19:30:27 +0300

    "preparing for user's manual 2"

--- app/src/main/java/com/example/finances/SplashScreenActivity.kt
+++ app/src/main/java/com/example/finances/SplashScreenActivity.kt
@@ -1,40 +1,48 @@
 package com.example.finances
 
+import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceManager
 
 class SplashScreenActivity : AppCompatActivity() {
-    val APP_PREFERENCES = "FirstEnter"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         val iv_note: ImageView by lazy { findViewById(R.id.rediss) }
         iv_note.alpha = 0f
-        iv_note.animate().setDuration(2000).alpha(1f).withEndAction(){
+        iv_note.animate().setDuration(2000).alpha(1f).withEndAction() {
 
 
             //обращаемся к настройкам
-            val getInfo = PreferenceManager.getDefaultSharedPreferences(this)
-            val EnterNum = getInfo.getString("FirstEnter", "")
+            val sp = getSharedPreferences(
+                "hasVisited", Context.MODE_PRIVATE
+            )
+            // проверяем, первый ли раз открывается программа (Если вход первый то вернет false)
+            val hasVisited = sp.getBoolean("hasVisited", false)
 
-            if (EnterNum.equals( "NotFirst")) {
-                    val i = Intent(this,MainActivity::class.java)
+
+                if (!hasVisited) {
+                    // Сработает если Вход первый
+                    //Ставим метку что вход уже был
+                    val e = sp.edit()
+                    e.putBoolean("hasVisited", true)
+                    e.apply()
+                    //Ниже запускаем активность которая нужна при первом входе
+                    val i = Intent(this, QuestionsActivity::class.java)
                     startActivity(i)
-                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-                    finish()}
-        else{
-                val Enter1 = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
-                var editor = Enter1.edit()
-                editor.putString(APP_PREFERENCES, "NotFirst").apply()
-            val i = Intent(this,QuestionsActivity::class.java)
-            startActivity(i)
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-            finish()
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                }
+                else {
+                    val i = Intent(this, MainActivity::class.java)
+                    startActivity(i)
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                }
+
         }
     }}
-}
